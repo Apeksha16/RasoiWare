6185e95513790feaf4315a37e1174f073c698ae0
Pranav Katiyar <pk.pranavkatiyar@gmail.com>
2023-08-01 11:31:49 +0530

    pushing carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'RasoiWare';
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   selectedValue: string = 'All Categories';
-  arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+  arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor(config: NgbDropdownConfig) {}
 
