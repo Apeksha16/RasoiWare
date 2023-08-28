602f091ac198745c847ff6b0622b24c134fae95e
Pranav Katiyar <pk.pranavkatiyar@gmail.com>
2023-08-28 23:10:18 +0530

    heroku setup index

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app>"));
+app.use(express.static(__dirname + "/dist/rasoiware"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/<name-of-app>/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/rasoiware/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
